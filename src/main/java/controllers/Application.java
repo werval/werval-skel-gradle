9473bf7ec46adc084a099dffa1d6e7dbf8cf58a2
Paul Merlin <paul@codeartisans.fr>
2014-05-07 00:20:26 +0200

    Add sample outcome in Application.java

--- src/main/java/controllers/Application.java
+++ src/main/java/controllers/Application.java
@@ -1,11 +1,13 @@
 package controllers;
 
 import org.qiweb.api.outcomes.Outcome;
+import static org.qiweb.api.context.CurrentContext.outcomes;
 
 public class Application {
 
     public Outcome index() {
         return new org.qiweb.api.controllers.Welcome().welcome();
+        // return outcomes().ok( "It works!" ).build();
     }
 
 }
